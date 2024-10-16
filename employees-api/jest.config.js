5beb1cd60b42c2f63e08fbe6796e2ceff8cfb2bf
Jose Alejandro Realza <josealejandror28@gmail.com>
2024-10-15 22:46:38 -0300

    [api] refactor: migrating tests folder into src

--- employees-api/jest.config.js
+++ employees-api/jest.config.js
@@ -1,19 +1,19 @@
 
 const config = {
-  roots: ['<rootDir>/src', '<rootDir>/test'],
+  roots: ['<rootDir>/src', '<rootDir>/src/__tests__'],
   "reporters": ["default"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
   testPathIgnorePatterns: [
     "/node_modules/",
-    "/test/utils/",
-    "/test/starterSetup.ts"
   ],
 
   collectCoverage: true,
   coverageDirectory: './_reporting/coverage',
   testTimeout: 1000 * 30,
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/__tests__/mocks/'],
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/__tests__/mocks/'],
 };
 
 module.exports = config;
